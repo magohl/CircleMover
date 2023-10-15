592e0245a66379bbf239c6dbbb05c24e665e9220
Magnus Ohlsson <magnus.ohlsson@gmail.com>
2023-10-15 08:45:55 +0200

    cleanup

--- circleMover.py
+++ circleMover.py
@@ -1,50 +1,28 @@
 import pygame
 
-# --- constants --- (UPPER_CASE names)
-
 SCREEN_WIDTH = 430
 SCREEN_HEIGHT = 410
 
-#BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+#WHITE = (255, 255, 255)
 RED   = (255,   0,   0)
 
 FPS = 30
 
-# --- classses --- (CamelCase names)
-
-# empty
-
-# --- functions --- (lower_case names)
-
-# empty
-
-# --- main ---
-
-# - init -
-
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#screen_rect = screen.get_rect()
 
-pygame.display.set_caption("Tracking System")
+pygame.display.set_caption("Circle Mover")
 
-# - objects -
-
-#rectangle = pygame.rect.Rect(176, 134, 17, 17)
-circle = pygame.draw.circle(screen, "blue", (100,100), 50)
-rectangle_draging = False
-
-# - mainloop -
+circle = pygame.draw.circle(screen, RED, (100,100), 50)
+circle_draging = False
 
 clock = pygame.time.Clock()
 
 running = True
 
 while running:
-
-    # - events -
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,38 +31,27 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:            
                 if circle.collidepoint(event.pos):
-                    rectangle_draging = True
+                    circle_draging = True
                     mouse_x, mouse_y = event.pos
                     offset_x = circle.x - mouse_x
                     offset_y = circle.y - mouse_y
 
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:            
-                rectangle_draging = False
+                circle_draging = False
 
         elif event.type == pygame.MOUSEMOTION:
-            if rectangle_draging:
+            if circle_draging:
                 mouse_x, mouse_y = event.pos
                 circle.x = mouse_x + offset_x
                 circle.y = mouse_y + offset_y
 
-    # - updates (without draws) -
+    screen.fill(BLACK)
 
-    # empty
-
-    # - draws (without updates) -
-
-    screen.fill(WHITE)
-
-#    pygame.draw.rect(screen, RED, rectangle)
     pygame.draw.circle(screen, RED, (circle.x, circle.y), 50)
 
     pygame.display.flip()
 
-    # - constant game speed / FPS -
-
     clock.tick(FPS)
-
-# - end -
 
 pygame.quit()
